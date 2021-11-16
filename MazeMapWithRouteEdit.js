7eca76fea315841ce767e9b1d9158fd726fab8bc
Matthew Templeton <47399002+tempy2000@users.noreply.github.com>
2021-11-16 12:39:55 +0000

    Add files via upload

--- MazeMapWithRouteEdit.js
+++ MazeMapWithRouteEdit.js
@@ -11,7 +11,7 @@ AFRAME.registerComponent('peakfinder', {
     _loadPeaks: function(longitude, latitude) {
        const scale = 2000;
        fetch("./buildings-data.json")
-       .then ( response => alert(JSON.stringify(response))
+       .then ( response => alert(JSON.stringify(response)))
        .catch(err =>alert(err));
        // .then(json => console.log(json));
        /*.then ( json => {
