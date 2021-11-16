8930f2ee2760768879dd100ead30128016160312
Matthew Templeton <47399002+tempy2000@users.noreply.github.com>
2021-11-16 11:12:59 +0000

    Test of AR.js Code

--- MazeMapWithRouteEdit.js
+++ MazeMapWithRouteEdit.js
@@ -1,12 +1,46 @@
+AFRAME.registerComponent('peakfinder', {
+    init: function() {
+        this.loaded = false;
+        window.addEventListener('gps-camera-update-position', e => {
+            if(this.loaded === false) {
+                this._loadPeaks(e.detail.position.longitude, e.detail.position.latitude);
+                this.loaded = true;
+            }
+        });
+    }
+    _loadPeaks: function(longitude, latitude) {
+       const scale = 2000;
+       fetch(`https://www.hikar.org/fm/ws/bsvr.php?bbox=${longitude-0.1},${latitude-0.1},${longitude+0.1},${latitude+0.1}&outProj=4326&format=json&poi=natural`
+           )
+       .then ( response => response.json() )
+       .then ( json => {
+           json.features.filter ( f => f.properties.natural == 'peak' )
+               .forEach ( peak => {
+                   const entity = document.createElement('a-text');
+                   entity.setAttribute('look-at', '[gps-projected-camera]');
+                   entity.setAttribute('value', peak.properties.name);
+                   entity.setAttribute('scale', {
+                       x: scale,
+                       y: scale,
+                       z: scale
+                   });
+                   entity.setAttribute('gps-projected-entity-place', {
+                       latitude: peak.geometry.coordinates[1],
+                       longitude: peak.geometry.coordinates[0]
+                   });
+                   this.el.appendChild(entity);
+               });
+       });
+   }
+});
+
 // The map
-
-
 //<!-- include aframe -->
 //<!-- with location based, use aframe v0.9.2 -->
 //include("https://aframe.io/releases/0.9.2/aframe.min.js")
 //<!-- include ar.js -->
 //include("https://raw.githack.com/AR-js-org/AR.js/master/aframe/build/aframe-ar-nft.js")
-
+/*
 const map = new Mazemap.Map({
     container: "map",
     campuses: 341,
@@ -17,15 +51,15 @@ const map = new Mazemap.Map({
 
 let lng;
 let lat;
-
+*/
 /*navigator.geolocation.getCurrentPosition(function(position){
   lng = position.coords.longitude;
   lat = position.coords.latitude;
-  
+
   console.log(lng);
   console.log(lat);
 })*/
-
+/*
 
 // Add map navigation controls
 map.addControl(new Mazemap.mapboxgl.NavigationControl());
@@ -45,7 +79,7 @@ map.on("load", () => {
 
     // Set the route
     //set_route(p1, p2);
-    
+    */
   /*const blueDot = new Mazemap.BlueDot({
     map : map,
   }).setLngLat({lng : lng, lat : lat})
@@ -53,23 +87,24 @@ map.on("load", () => {
     .setAccuracy(10)
     .show();
   */
-              
+
     //BlueDot
+    /*
   const blueDot = new Mazemap.BlueDot({
       map : map
   })
      .setAccuracy(10)
      .show();
-    
+
   var locationController = new Mazemap.LocationController({
     blueDot: blueDot,
     map: map
   });
-  locationController.setState('follow'); 
+  locationController.setState('follow');
   const watchId = navigator.geolocation.watchPosition(position => {
-      
+
     const { latitude, longitude } = position.coords;
-    
+
     var testElement = document.querySelector("body > ascene > a-text")
 
     testElement.setAttribute("gps-entity-place", "latitude: " + latitude + "; longitude: " + longitude + ";")
@@ -80,12 +115,12 @@ map.on("load", () => {
             lat: latitude
         }
     })
-      
+
     if(trigger) {
         set_route({ lngLat: { lng: longitude, lat: latitude }, zLevel: map.zLevel }, p2);
         trigger = false;
     }
-  });
+  });*/
     //map.flyTo({center:{lng : longitude, lat : latitude}, zoom: 18});
     // Show a map centered at latitude / longitude.
 
@@ -93,8 +128,7 @@ map.on("load", () => {
     lng = position.coords.longitude;
     lat = position.coords.latitude;
   */
-
-  
+/*
 });
 
 function set_route(p1, p2) {
@@ -113,6 +147,5 @@ function set_route(p1, p2) {
         map.fitBounds(bounds, { padding: 100 });
     });
 };
-
+*/
 // Get and set the route
-
