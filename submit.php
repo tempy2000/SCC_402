8bbdb35b9596d687543260c3a807643d85c31073
Dan Barton <danbarton2000@gmail.com>
2021-12-07 10:57:29 +0000

    Added json file type to file

--- submit.php
+++ submit.php
@@ -1,7 +1,7 @@
 <?php 
     header("Content-Type: application/json");
     $data = json_decode(file_get_contents("php://input"));
-    $file = fopen("logs/" . $data->start, "w");
+    $file = fopen("logs/" . $data->start . ".json", "w");
     fwrite($file, $data);
     fclose($file);
 ?>
